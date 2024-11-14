f902fdfa148eba92a2177fd34a4a8644f00d2b8d
Aen04 <aeniz@comunidad.unnoba.edu.ar>
2024-11-14 13:34:41 -0300

    Update pageRender.java

--- pageRender.java
+++ pageRender.java
@@ -8,5 +8,28 @@ public class PageRender<T>{
   private Page<T> page;
   private int totalPaginas;
   private int paginaActual;
-  private List<PageItem>;
+  private List<PageItem> paginas;
+
+  public PageRender(string url, Page<T> page){
+    this.url = url;
+    this.page = page;
+    this.paginas = new ArrayList<PageItem>();
+
+    numElementosPorPagina = 5;
+    totalPaginas = page.getTotalPages();
+    paginaActual = page.getNumber();
+
+    int desde , hasta;
+    if(totalPaginas <= numElementosPorPagina){
+      desde = 1;
+      hata = totalPaginas;
+    }else{
+      if(paginaActual <= numElementoPorPagina/2){
+        desde = 1;
+        hasta = numElementosPorPagina;
+      }
+    }
+  }
+
+  
 }
