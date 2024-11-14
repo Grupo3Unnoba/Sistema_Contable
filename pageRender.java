fed0f16a86fd18d4fc694b03d230ef891efe28df
Aen04 <aeniz@comunidad.unnoba.edu.ar>
2024-11-14 09:46:06 -0300

    Create pageRender.java
    YT

---
+++ pageRender.java
@@ -0,0 +1,12 @@
+package com.gestion.empleados.util.paginacion;
+
+import java.util.ArrayList; 
+
+public class PageRender<T>{
+
+  private String url;
+  private Page<T> page;
+  private int totalPaginas;
+  private int paginaActual;
+  private List<PageItem>;
+}
