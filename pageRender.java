47b8cbabf12b8a99f365f2f93d1ecae2cf121f07
Aen04 <aeniz@comunidad.unnoba.edu.ar>
2024-11-14 14:07:38 -0300

    Update pageRender.java

--- pageRender.java
+++ pageRender.java
@@ -1,6 +1,6 @@
 package com.gestion.empleados.util.paginacion;
 
-import java.util.ArrayList; 
+import java.util.ArrayList;                       
 
 public class PageRender<T>{
 
@@ -11,8 +11,8 @@ public class PageRender<T>{
   private List<PageItem> paginas;
 
   public PageRender(string url, Page<T> page){
-    this.url = url;
-    this.page = page;
+    this.url = url;                      
+    this.page = page;                      
     this.paginas = new ArrayList<PageItem>();
 
     numElementosPorPagina = 5;
@@ -23,13 +23,65 @@ public class PageRender<T>{
     if(totalPaginas <= numElementosPorPagina){
       desde = 1;
       hata = totalPaginas;
-    }else{
-      if(paginaActual <= numElementoPorPagina/2){
+    }else{                                              
+      if(paginaActual <= numElementosPorPagina/2){
         desde = 1;
         hasta = numElementosPorPagina;
       }
+      else if(paginaActual >= totalPaginas - numElementosPorPagina + 1){
+        desde = totalPaginas - numElementosPorPagina + 1;
+        hasta = numElementosPorPagina;
+      }else{
+        desde = paginaActual - numElementosPorPagina/2;
+        hasta = numElementosPorPagina;
+      }
+    }
+    for(int i = 0; i < hasta; i++){
+      paginas.add(new PageItem(desde + i, paginaActual == desde + i));
     }
   }
+  public String getUrl(){
+    return url;
+  }
+  
+  public void setUrl(String url){
+    this.url = url;
+  }
+  
+  public int getTotalPaginas(){
+    return totalPaginas;
+  }
 
+  public void setTotalPaginas(int totalPaginas){
+    this.totalPaginas = totalPaginas;
+  }
+
+  public int getPaginaActual(int paginaActual){
+    this.paginaActual = paginaActual;
+  }
+
+  public void setPaginaActual(int paginaActual){
+    this.paginaActual = paginaActual;
+  }
+
+  public List<PageItem> getPaginas(){
+    return paginas;
+  }
+
+  public void setPaginas(List<PageItem> paginas) {
+    this.paginas = paginas;
+  }
+
+  public boolean isHasNext(){
+    return page.isLast();
+  }
+
+  public boolean isHasNext(){
+    return page.hasNext();
+  }
+
+  public boolean isHasPrevius(){
+    return page.hasPrevius();
+  }
   
 }
